2e245207465077dad6042491db7be8cf45f28df7
Yeshasvi Tirupachuri <yeshasvitvs@gmail.com>
2019-08-21 20:59:31 +0200

    Update the name of human config file

--- retargeting-shell-scripts/retargeting-yarprobotinterface.sh
+++ retargeting-shell-scripts/retargeting-yarprobotinterface.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xterm -title "Retargeting Human" -e "yarprobotinterface --config Human.xml" &
+xterm -title "Retargeting Human" -e "yarprobotinterface --config RobotStateProvider_Human.xml" &
 xterm -title "Retargeting Nao" -e "yarprobotinterface --config RobotStateProvider_Nao.xml" &
 xterm -title "Retargeting iCub" -e "yarprobotinterface --config RobotStateProvider_iCub.xml" &
 xterm -title "Retargeting Walkman" -e "yarprobotinterface --config RobotStateProvider_Walkman.xml" &
