a3c4963d989776f0cfa9d21bc61364690d059fea
Yeshasvi Tirupachuri <yeshasvitvs@gmail.com>
2019-08-23 16:23:30 +0200

    Comment out nao and baxter from bash scripts and rviz

--- retargeting-shell-scripts/retargeting-yarprobotinterface.sh
+++ retargeting-shell-scripts/retargeting-yarprobotinterface.sh
@@ -2,8 +2,8 @@
 
 xterm -title "Transform Server" -e "yarprobotinterface --config TransformServer.xml" &
 xterm -title "Retargeting Human" -e "yarprobotinterface --config RobotStateProvider_Human.xml" &
-xterm -title "Retargeting Nao" -e "yarprobotinterface --config RobotStateProvider_Nao.xml" &
 xterm -title "Retargeting iCub" -e "yarprobotinterface --config RobotStateProvider_iCub.xml" &
 xterm -title "Retargeting Walkman" -e "yarprobotinterface --config RobotStateProvider_Walkman.xml" &
 xterm -title "Retargeting Atlas" -e "yarprobotinterface --config RobotStateProvider_Atlas.xml" &
-xterm -title "Retargeting Baxter" -e "yarprobotinterface --config RobotStateProvider_Baxter.xml" &
+#xterm -title "Retargeting Nao" -e "yarprobotinterface --config RobotStateProvider_Nao.xml" &
+#xterm -title "Retargeting Baxter" -e "yarprobotinterface --config RobotStateProvider_Baxter.xml" &
